edb5427e7af1d93c7448a132635ace54102c8ffa
gaurav4040 <jangragaurav593@gmail.com>
2025-01-20 17:21:53 +0530

    x

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,47 +1,42 @@
-import { useState } from "react";
+import { useState,useRef } from "react";
 import styles from "./AddTodo.module.css"
 
 export default function AddTodo({handleInput}) {
 
-    let [TaskValue,setTaskValue] = useState();
-    let [dateValue,setDateValue] = useState();
-
-    const inputTask=(event)=>{
-       
-         setTaskValue(event.target.value);
-        
-    }
-    const inputDate=(event)=>{
-           setDateValue(event.target.value);
-    }
-    const addButtonClicked=()=>{
-        handleInput(TaskValue,dateValue);
-        setTaskValue("");
-        setDateValue("");
+    let todoName=useRef();
+    let todoDate=useRef();
+ 
+    const addButtonClicked=(event)=>{
+        event.preventDefault();
+        const name=todoName.current.value;
+        const date=todoDate.current.value;
+        handleInput(name,date);
     }
 
     return (
     <div className={`container text-center ${styles.inputContainer}`}>
-        <div className={`row ${styles.inputContainer}`}>
+        <form className={`row ${styles.inputContainer}`}>
             <div className={`col-6 ${styles.inputContainer}`}>
                 <input  type="text"
+                ref={todoName}
                 className={`${styles.inputbox} form-control me-2`}
                  placeholder="Enter the task" 
                  value={TaskValue}
-                 onChange={(event)=>{inputTask(event)}} />
+                  />
             </div>
             <div className={`col-4 ${styles.inputContainer}`}>
                 <input  type="date"
+                ref={todoDate}
                  className={`${styles.inputbox} ${styles.date} form-control me-2`}
                  value={dateValue} 
-                 onChange={(event)=>{inputDate(event)}} />
+                 />
             </div>
             <div className={`col-2 ${styles.inputContainer}`}>
                 <button 
                   className={`${styles["Gbutton"]} btn btn-success`}
                   onClick={addButtonClicked} >Add</button>
             </div>
-        </div>
+        </form>
     </div>
     )
 }
